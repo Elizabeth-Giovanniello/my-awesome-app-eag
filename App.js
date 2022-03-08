c318819a1a8c5e7a0f94f8a51ffdfa8a39e77eac
Lizzy <ea.giovanniello@gmail.com>
2022-03-08 12:18:27 -0500

    docs: added clarifying comment.

--- App.js
+++ App.js
@@ -8,3 +8,5 @@ const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
 app.listen(APP_PORT, () => {
     console.log(`Running app at port:${APP_PORT}`)
 })
+
+//this project is to practice using branches, tags, and releases for github
